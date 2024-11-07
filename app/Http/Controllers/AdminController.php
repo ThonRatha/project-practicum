e5bc597baafb5756e7cfa304e85c6d4469e9cb19
ThonRatha <rttr3888@gmail.com>
2024-11-07 16:41:54 +0700

    login part

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard() {
 
-        return view('admin.admin_dashboard');
-        
+        return view('admin.index');
+
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+    public function AdminLogin() {
+        return view('admin.admin_login');
     }
 }
