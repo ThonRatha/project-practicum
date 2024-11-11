6ca62e4d2e9ef4115096ba78d276aaed170a6357
ThonRatha <rttr3888@gmail.com>
2024-11-11 15:51:44 +0700

    Admin Profile Part

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -33,5 +34,69 @@ class AdminController extends Controller
         $id = Auth::user()->id;
         $profileData = User::find($id);
         return view('admin.admin_profile_view', compact('profileData'));
+    }
+
+    public function AdminProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        // check if it have img or not
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            // removing existing img (when update img it will be replace new img)
+            @unlink(public_path('upload/admin_images/'.$data->photo));
+            // generated year, month, date and get of img ex. 123421.avatar-2.png
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+
+        // message notification
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    }
+
+    public function AdminChangePassword(){
+        $id = Auth::user()->id;
+        //find specific row id
+        $profileData = User::find($id);
+        return view('admin.admin_change_password', compact('profileData'));
+    }
+
+    public function AdminPasswordUpdate(Request $request){
+        // validation area
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if(!Hash::check($request->old_password, auth::user()->password)){
+            $notification = array(
+                'message' => 'Old Password Does Not Match!',
+                'alert-type' => 'error'
+            );
+
+            return redirect()->back()->with($notification);
+        }
+
+        // update the new password
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
     }
 }
--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -11,7 +11,7 @@
                 <ol class="breadcrumb mb-0 p-0">
                     <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page">Admin Profile</li>
+                    <li class="breadcrumb-item active" aria-current="page">Admin Change Password</li>
                 </ol>
             </nav>
         </div>
@@ -67,60 +67,43 @@
                 </div>
                 <div class="col-lg-8">
                     <div class="card">
-                        <form action="{{ route('admin.profile.store') }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('admin.password.update') }}" method="POST" enctype="multipart/form-data">
                             @csrf
 
                             <div class="card-body">
                                 <div class="row mb-3">
                                     <div class="col-sm-3">
-                                        <h6 class="mb-0">Name</h6>
+                                        <h6 class="mb-0">Old Password</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" name="name" class="form-control" value="{{ $profileData->name }}" />
+                                        <input type="text" name="old_password" class="form-control @error('old_password') is-invalid
+                                        @enderror" id="old_password"/>
+                                        @error('old_password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <div class="col-sm-3">
-                                        <h6 class="mb-0">Email</h6>
+                                        <h6 class="mb-0">New Password</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" name="email" class="form-control" value="{{ $profileData->email }}" />
+                                        <input type="text" name="new_password" class="form-control @error('old_password') is-invalid
+                                        @enderror" id="old_password"/>
+                                        @error('new_password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <div class="col-sm-3">
-                                        <h6 class="mb-0">Phone</h6>
+                                        <h6 class="mb-0">Confirm Password</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" name="phone" class="form-control" value="{{ $profileData->phone }}" />
+                                        <input type="text" name="new_password_confirmation" class="form-control @error('old_password') is-invalid
+                                        @enderror" id="new_password_confirmation"/>
                                     </div>
                                 </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Address</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" name="address" class="form-control" value="{{ $profileData->address }}" />
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Photo</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="file" name="photo" class="form-control" id="image" />
-                                    </div>
-                                </div>
-
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0"></h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
-                                    </div>
-                                </div>
-
                                 <div class="row">
                                     <div class="col-sm-3"></div>
                                     <div class="col-sm-9 text-secondary">
@@ -136,17 +119,5 @@
         </div>
     </div>
 </div>
-
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('#image').change(function (e) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#showImage').attr('src', e.target.result);
-            }
-            reader.readAsDataURL(e.target.files[0]);
-        });
-    });
-</script>
 
 @endsection
