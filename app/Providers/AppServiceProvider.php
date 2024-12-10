5325084a17ed8403946c352c8ef665eb18d11e88
ThonRatha <rttr3888@gmail.com>
2024-12-10 21:47:37 +0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Intervention\Image\Facades\Image;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,6 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });
